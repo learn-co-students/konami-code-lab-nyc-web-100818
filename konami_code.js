e4fd9b8f4ec015c50c3055bb421fccf2733958de
magic-onion <alexander.m.truong@gmail.com>
2018-11-20 18:14:32 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0
+
+  document.body.addEventListener("keydown", function (event) {
+    const key = event.key
+
+    if (key === codes[index]) {
+      index++
+    }
+    else {
+      index = 0
+    }
+
+    if (index === codes.length) {
+      window.alert("Ayy!")
+      index = 0
+    }
+  });
 }
